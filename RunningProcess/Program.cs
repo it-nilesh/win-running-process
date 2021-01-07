e43f83b38ce51d8a9496cda6f3cc2a06e09d869d
Nilesh <it.nilesh.25@gmail.com>
2021-01-07 23:08:17 +0530

    init project

---
+++ RunningProcess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace RunningProcess
+{
+    //https://stackoverflow.com/questions/40070703/how-to-get-a-list-of-open-tabs-from-chrome-c-sharp
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processes = Process.GetProcesses().Where(x => !string.IsNullOrEmpty(x.MainWindowTitle));
+            foreach (Process p in processes)
+            {
+                Console.WriteLine($"{p.ProcessName} -- {p.MainWindowTitle}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
